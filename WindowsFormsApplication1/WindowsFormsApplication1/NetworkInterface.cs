07e5403074f359817d922969d9a517382806ac8c
tarrenj <jake.tarren@gmail.com>
2015-11-23 20:16:27 -0500

    Initial project creation
    Creates the project in Visual Studio.
    Create the NetworkInterface and Switch class.

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/NetworkInterface.cs
@@ -0,0 +1,35 @@
+﻿class NetworkInterface
+{
+    public string Name { get; }
+    public bool IsConnected => Link != null;
+    public NetworkInterface Link { get; private set; }
+
+    public NetworkInterface(string name)
+    {
+        Name = name;
+    }
+
+    private void ConnectToInternal(NetworkInterface networkInterface)
+    {
+        // Seporate method for setting Link variable to make it easier to bidirectionally link NetworkInterfaces
+        Link = networkInterface;
+    }
+
+    public void SetConnection(NetworkInterface networkInterface)
+    {
+        if (networkInterface == null)
+        {
+            throw new System.ArgumentNullException("Can't connect to null network interface", nameof(networkInterface));
+        }
+
+        ConnectToInternal(networkInterface);
+        networkInterface.ConnectToInternal(this);
+    }
+
+    public void Disconnect()
+    {
+        // Only call ConnectToInternal on Link if Link != null ... Basic error handeling
+        Link?.ConnectToInternal(null);
+        Link = null;
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Switch.cs
@@ -0,0 +1,15 @@
+﻿class Switch
+{
+    public string SwitchName { get; }
+    public NetworkInterface[] Ports { get; }
+    public int PortCount { get; }
+
+    public Switch(string name, int PortNum)
+    {
+        SwitchName = name;
+        PortCount = PortNum;
+
+        for (int i = 0; i < PortCount; i++)
+        { Ports[i] = new NetworkInterface(SwitchName + "_interface_" + i); }
+    }
+}
